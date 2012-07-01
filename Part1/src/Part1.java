1641b95a883e11ee0194b783fa5ddd48bb91837a
Richard Chatterton <rebirthed84@juno.com>
2012-07-01 15:01:05 -0700

    Menu in Part1 updated
    Massive update for menu for easier user-friendly interface, no loop yet,
    though

--- Part1/src/Part1.java
+++ Part1/src/Part1.java
@@ -14,7 +14,7 @@ public class Part1 {
 
 		Scanner kbPrintAll = new Scanner(System.in);
 		Scanner kbPrintNumber = new Scanner(System.in);
-		Scanner kbPrintType = new Scanner(System.in);
+		Scanner kbPrintChoice = new Scanner(System.in);
 
 		// Main menu interface
 
@@ -36,23 +36,42 @@ public class Part1 {
 				.println("--------------------------------DASHBOARD------------------------------------\n");
 		System.out
 				.println("=============================================================================");
-		System.out.println("FILTER                    |		SELECTION");
+		System.out.println("SELECTION                 |		COMMAND");
+		System.out.println("                          |");
+		System.out
+				.println("-----------------------------------------------------------------------------");
+		System.out.println("Press 1:        	  |    	List all Vehicles");
 		System.out.println("                          |");
 		System.out
 				.println("-----------------------------------------------------------------------------");
 		System.out
-				.println("List all Vehicles?        |     ------> Press 'Y' or 'N'");
+				.println("-----------------------------------------------------------------------------");
 		System.out.println("                          |");
+		System.out.print("Press 2:     		  | 	List Vehicle by Type");
+		System.out
+				.println("\n-----------------------------------------------------------------------------");
+
 		System.out
 				.println("-----------------------------------------------------------------------------");
+		System.out.println("                          |");
+		System.out.print("Press 3:     		  | 	List Number of Vehicles by Type");
+		System.out
+				.println("\n-----------------------------------------------------------------------------");
+
+		System.out
+				.println("-----------------------------------------------------------------------------");
+		System.out.println("                          |");
+		System.out.print("Press 4:     		  | 	Quit program");
+		System.out
+				.println("\n-----------------------------------------------------------------------------");
 
 		/*
-		 * PRINT ALL VEHICLES
+		 * OPTION 1: PRINT ALL VEHICLES
 		 */
 
-		String printAllChoice = kbPrintAll.nextLine();
+		String printChoice = kbPrintChoice.nextLine();
 
-		if (printAllChoice.equalsIgnoreCase("y")) {
+		if (printChoice.equalsIgnoreCase("1")) {
 			/*
 			 * PRINT ALL CARS
 			 */
@@ -88,95 +107,156 @@ public class Part1 {
 
 			theCompany.printVehicles(VehicleType.Bike);
 
-		} // if
-		System.out
-				.println("\n*****************************************************************************");
+			System.out
+					.println("\n*****************************************************************************");
+			
+			/*
+			 * OPTION 2: PRINT VEHICLES BY TYPE
+			 */
+			
+
+		} else if (printChoice.equalsIgnoreCase("2")) {
+			Scanner kbPrintType = new Scanner(System.in);
+			System.out.println("Which type? (Type 'Car,' 'Truck,' or 'Bike')");
+			String printType = kbPrintType.nextLine();
+			
+			// PRINT CAR TYPE
+
+			if (printType.equalsIgnoreCase("Car")) {
+				System.out
+						.println("*****************************************************************************");
+				System.out.println("		Types of Cars for rent: ");
+				System.out
+						.println("*****************************************************************************");
+				theCompany.printVehicles(VehicleType.Car);
+				System.out
+						.println("*****************************************************************************");
+			} else if (printType.equalsIgnoreCase("Truck")) {
+				System.out
+						.println("*****************************************************************************");
+				System.out.println("		Types of Trucks for rent: ");
+				System.out
+						.println("*****************************************************************************");
+				theCompany.printVehicles(VehicleType.Truck);
+				System.out
+						.println("*****************************************************************************");
+			} else if (printType.equalsIgnoreCase("Bike")) {
+				System.out
+						.println("*****************************************************************************");
+				System.out.println("		Types of Bikes for rent: ");
+				System.out
+						.println("*****************************************************************************");
+				theCompany.printVehicles(VehicleType.Bike);
+				System.out
+						.println("*****************************************************************************");
+			}
+
+			/*
+			 * OPTION 3: LIST NUMBER OF VEHICLES
+			 */
+			
+		} else if (printChoice.equalsIgnoreCase("3")) {
+			System.out.println("Which type? (Type 'Car,' 'Truck,' or 'Bike')");
+			String printNumber = kbPrintNumber.nextLine();
+			
+			if (printNumber.equalsIgnoreCase("Car")) {
+				 System.out.println("*****************************************************************************");
+				 System.out.println("		Number of CARS for rent: " + theCompany.getTotal(VehicleType.Car)); 
+				 System.out .println("*****************************************************************************");
+			} else if (printNumber.equalsIgnoreCase("Truck")) {
+				 System.out.println("*****************************************************************************");
+				 System.out.println("		Number of TRUCKS for rent: " + theCompany.getTotal(VehicleType.Truck)); 
+				 System.out .println("*****************************************************************************");
+			} else if (printNumber.equalsIgnoreCase("Bike")) {
+				 System.out.println("*****************************************************************************");
+				 System.out.println("		Number of BIKES for rent: " + theCompany.getTotal(VehicleType.Bike)); 
+				 System.out .println("*****************************************************************************");
+		}		
+
+		/*
+		 * OPTION 4: QUIT PROGRAM
+		 */
+
+		if (printChoice.equalsIgnoreCase("4")) {
+			System.exit(0);
+		}
 
 		/*
 		 * PRINT ALL VEHICLES OF A TYPE
 		 */
 
-		System.out
-				.println("-----------------------------------------------------------------------------");
-		System.out.println("                          |");
-		System.out.print("List Vehicle by Type:     |");
-		System.out.print("	[ Cars ] [ Trucks ] [ Bikes ]");
-		System.out
-				.println("\n-----------------------------------------------------------------------------");
-		String printType = kbPrintType.nextLine();
-
-		if (printType.equalsIgnoreCase("Cars")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Types of Cars for rent: ");
-			System.out
-					.println("*****************************************************************************");
-			theCompany.printVehicles(VehicleType.Car);
-			System.out
-					.println("*****************************************************************************");
-		}
-
-		else if (printType.equalsIgnoreCase("trucks")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Types of Trucks for rent: ");
-			System.out
-					.println("*****************************************************************************");
-			theCompany.printVehicles(VehicleType.Truck);
-			System.out
-					.println("*****************************************************************************");
-		}
-
-		else if (printType.equalsIgnoreCase("bikes")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Types of Bikes for rent: ");
-			System.out
-					.println("*****************************************************************************");
-			theCompany.printVehicles(VehicleType.Bike);
-			System.out
-					.println("*****************************************************************************");
-		}
-
 		/*
-		 * PRINT NUMBER OF VEHICLES OF A TYPE
+		 * System.out .println(
+		 * "-----------------------------------------------------------------------------"
+		 * ); System.out.println("                          |");
+		 * System.out.print("List Vehicle by Type:     |");
+		 * System.out.print("	[ Cars ] [ Trucks ] [ Bikes ]"); System.out
+		 * .println(
+		 * "\n-----------------------------------------------------------------------------"
+		 * ); String printType = kbPrintType.nextLine();
+		 * 
+		 * if (printType.equalsIgnoreCase("Cars")) { System.out .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Types of Cars for rent: "); System.out
+		 * .println(
+		 * "*****************************************************************************"
+		 * ); theCompany.printVehicles(VehicleType.Car); System.out .println(
+		 * "*****************************************************************************"
+		 * ); }
+		 * 
+		 * else if (printType.equalsIgnoreCase("trucks")) { System.out .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Types of Trucks for rent: "); System.out
+		 * .println(
+		 * "*****************************************************************************"
+		 * ); theCompany.printVehicles(VehicleType.Truck); System.out .println(
+		 * "*****************************************************************************"
+		 * ); }
+		 * 
+		 * else if (printType.equalsIgnoreCase("bikes")) { System.out .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Types of Bikes for rent: "); System.out
+		 * .println(
+		 * "*****************************************************************************"
+		 * ); theCompany.printVehicles(VehicleType.Bike); System.out .println(
+		 * "*****************************************************************************"
+		 * ); }
+		 * 
+		 * /* PRINT NUMBER OF VEHICLES OF A TYPE
+		 * 
+		 * 
+		 * System.out .println(
+		 * "-----------------------------------------------------------------------------"
+		 * ); System.out.println("                          |");
+		 * System.out.print("List Vehicle by Type:     |");
+		 * System.out.print("	[ Cars ] [ Trucks ] [ Bikes ]"); System.out
+		 * .println(
+		 * "\n-----------------------------------------------------------------------------"
+		 * ); String printNumber = kbPrintNumber.nextLine();
+		 * 
+		 * if (printNumber.equalsIgnoreCase("Cars")) { System.out .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Number of CARS for rent: " +
+		 * theCompany.getTotal(VehicleType.Car)); System.out .println(
+		 * "*****************************************************************************"
+		 * ); }
+		 * 
+		 * else if (printNumber.equalsIgnoreCase("trucks")) { System.out
+		 * .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Number of TRUCKS for rent: " +
+		 * theCompany.getTotal(VehicleType.Truck)); System.out .println(
+		 * "*****************************************************************************"
+		 * ); } else if (printNumber.equalsIgnoreCase("bikes")) { System.out
+		 * .println(
+		 * "*****************************************************************************"
+		 * ); System.out.println("		Number of BIKES for rent: " +
+		 * theCompany.getTotal(VehicleType.Bike)); System.out .println(
+		 * "*****************************************************************************"
+		 * );
+		 * 
+		 * }
 		 */
-
-		System.out
-				.println("-----------------------------------------------------------------------------");
-		System.out.println("                          |");
-		System.out.print("List Vehicle by Type:     |");
-		System.out.print("	[ Cars ] [ Trucks ] [ Bikes ]");
-		System.out
-				.println("\n-----------------------------------------------------------------------------");
-		String printNumber = kbPrintNumber.nextLine();
-
-		if (printNumber.equalsIgnoreCase("Cars")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Number of CARS for rent: "
-					+ theCompany.getTotal(VehicleType.Car));
-			System.out
-					.println("*****************************************************************************");
-		}
-
-		else if (printNumber.equalsIgnoreCase("trucks")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Number of TRUCKS for rent: "
-					+ theCompany.getTotal(VehicleType.Truck));
-			System.out
-					.println("*****************************************************************************");
-		}
-
-		else if (printNumber.equalsIgnoreCase("bikes")) {
-			System.out
-					.println("*****************************************************************************");
-			System.out.println("		Number of BIKES for rent: "
-					+ theCompany.getTotal(VehicleType.Bike));
-			System.out
-					.println("*****************************************************************************");
-		}
 	}
-
+	}
 }
